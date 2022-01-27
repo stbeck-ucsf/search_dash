07dba3c401db59d6804bc4c4be3b39eb2a4d3eb2
stbeck-ucsf <stephen.beck@ucsf.edu>
2022-01-27 06:28:50 -0800

    Init generic dashboard MVP

---
+++ search_dash/search_dash.py
@@ -0,0 +1,24 @@
+# %%
+# dash libs
+import dash
+import dash_bootstrap_components as dbc
+import plotly.express as px
+from dash import Input, Output, dcc, html
+from dash_extensions import Mermaid
+from tableone import TableOne
+
+# data libs
+import pandas as pd
+from mcr_toolkit.dataset_tools import fetch_df
+
+# %%
+# Data Manipulation
+
+
+# %%
+# Define Components
+
+
+# %%
+# App Layout
+
